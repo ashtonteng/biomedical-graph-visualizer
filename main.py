2268c2a47dd924084d07b2ecb6c5c371f2aead27
ashtonteng <ashtonteng@gmail.com>
2020-05-02 12:09:39 -0700

    added constants v1, updated variable names in main.py

--- main.py
+++ main.py
@@ -2,20 +2,12 @@ from downloader import query
 from queries.templates import *
 from queries.constants import *
 
-TAXON, NO_TAXON, WILDCARD = (query_template_taxon, query_template_no_taxon, query_template_wildcard)
-main_concept_id_dict = {GENE: GENE_ID, PROTEIN: PROTEIN_ID}
-peripheral_concept_id_dict = {PROTEIN_FAMILY: PROTEIN_FAMILY_ID}
-all_concept_id_dict = dict()
-all_concept_id_dict.update(main_concept_id_dict)
-all_concept_id_dict.update(peripheral_concept_id_dict)
-relation_id_dict = {PART_OF: PART_OF_ID}
-edges_dict = {(PROTEIN, PROTEIN_FAMILY): (PART_OF, TAXON)}
 
 def download_all_data():
     print("Downloading....")
-    for (core_concept, periphery_concept) in edges_dict:
-        relation, query_template = edges_dict[(core_concept, periphery_concept)]
-        q = query_template(main_concept_id_dict[core_concept], relation_id_dict[relation])
+    for (core_concept, periphery_concept) in EDGES_DICT:
+        relation, query_template = EDGES_DICT[(core_concept, periphery_concept)]
+        q = query_template(CONCEPT_ID_DICT[core_concept], RELATION_ID_DICT[relation])
         
         print(q)
         
