94ec678ebf7cd8905f533c5346e935a5209d5bb7
Mars <marshuang80@gmail.com>
2020-05-04 13:46:07 -0700

    [EDIT] format

--- downloader.py
+++ downloader.py
@@ -2,24 +2,24 @@ from SPARQLWrapper import SPARQLWrapper, JSON
 import time
 
 endpoint = SPARQLWrapper("https://query.wikidata.org/bigdata/namespace/wdq/sparql")
-wd = 'PREFIX wd: <http://www.wikidata.org/entity/>'
-wdt = 'PREFIX wdt: <http://www.wikidata.org/prop/direct/>'
+wd = "PREFIX wd: <http://www.wikidata.org/entity/>"
+wdt = "PREFIX wdt: <http://www.wikidata.org/prop/direct/>"
+
 
 def query(query):
-  endpoint.setQuery(query)
-  endpoint.setReturnFormat(JSON)
-  result = None
-  while result is None:
-    try:
-      result = endpoint.query().convert()
-    except:
-      print("HTTP Timeout! Sleeping...")
-      time.sleep(5)
-  return result
+    endpoint.setQuery(query)
+    endpoint.setReturnFormat(JSON)
+    result = None
+    while result is None:
+        try:
+            result = endpoint.query().convert()
+        except:
+            print("HTTP Timeout! Sleeping...")
+            time.sleep(5)
+    return result
 
-  def get_query_for_gene():
-    q = \
-        '''SELECT ?gene ?geneLabel ?association ?associationLabel ?protein ?proteinLabel ?chromosome ?chromosomeLabel ?substitution ?substitutionLabel ?strand ?strandLabel WHERE {
+    def get_query_for_gene():
+        q = """SELECT ?gene ?geneLabel ?association ?associationLabel ?protein ?proteinLabel ?chromosome ?chromosomeLabel ?substitution ?substitutionLabel ?strand ?strandLabel WHERE {
         ?gene (wdt:P279|wdt:P31) wd:Q7187;
                 wdt:P703 wd:Q15978631.
         
@@ -30,5 +30,5 @@ def query(query):
         OPTIONAL {?gene wdt:P1916 ?substitution.}
         
         SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en". }
-        }'''
-    return q
+        }"""
+        return q
--- queries/templates.py
+++ queries/templates.py
@@ -1,26 +1,35 @@
 # This file provides templates for sparql queries
 
+
 def query_template_taxon(concept, property):
-  query = "SELECT ?concept ?conceptLabel ?peripheralConcept ?peripheralConceptLabel WHERE { " + \
-          f"?concept (wdt:P279|wdt:P31) wd:{concept}; " + \
-                  "wdt:P703 wd:Q15978631; " + \
-                  f"wdt:{property} ?peripheralConcept. " + \
-          '''SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en". } ''' + \
-          "}"
-  return query
+    query = (
+        "SELECT ?concept ?conceptLabel ?peripheralConcept ?peripheralConceptLabel WHERE { "
+        + f"?concept (wdt:P279|wdt:P31) wd:{concept}; "
+        + "wdt:P703 wd:Q15978631; "
+        + f"wdt:{property} ?peripheralConcept. "
+        + """SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en". } """
+        + "}"
+    )
+    return query
+
 
 def query_template_no_taxon(concept, property):
- query = "SELECT ?concept ?conceptLabel ?peripheralConcept ?peripheralConceptLabel WHERE { " + \
-        f"?concept (wdt:P279|wdt:P31) wd:{concept}; " + \
-                f"wdt:{property} ?peripheralConcept. " + \
-        '''SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en". } ''' + \
-        "}"
- return query
+    query = (
+        "SELECT ?concept ?conceptLabel ?peripheralConcept ?peripheralConceptLabel WHERE { "
+        + f"?concept (wdt:P279|wdt:P31) wd:{concept}; "
+        + f"wdt:{property} ?peripheralConcept. "
+        + """SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en". } """
+        + "}"
+    )
+    return query
+
 
 def query_template_wildcard(concept, property):
- query = "SELECT ?concept ?conceptLabel ?peripheralConcept ?peripheralConceptLabel WHERE { " + \
-        f"?concept (wdt:P279|wdt:P31)* wd:{concept}; " + \
-                f"wdt:{property} ?peripheralConcept. " + \
-        '''SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en". } ''' + \
-        "}"
- return query
+    query = (
+        "SELECT ?concept ?conceptLabel ?peripheralConcept ?peripheralConceptLabel WHERE { "
+        + f"?concept (wdt:P279|wdt:P31)* wd:{concept}; "
+        + f"wdt:{property} ?peripheralConcept. "
+        + """SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en". } """
+        + "}"
+    )
+    return query
