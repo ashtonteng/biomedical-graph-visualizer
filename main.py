9990e5de37b2988db9e413d1a895ac8ca48f17ab
Hell Walker <ashtonteng@gmail.com>
2020-05-03 23:33:19 -0700

    modified testing in main.py, commented out protein superfamily due to redundancy with protein family

--- main.py
+++ main.py
@@ -8,13 +8,15 @@ def download_all_data():
     for (core_concept, periphery_concept) in EDGES_DICT:
         relation, query_template = EDGES_DICT[(core_concept, periphery_concept)]
         q = query_template(CONCEPT_ID_DICT[core_concept], RELATION_ID_DICT[relation])
-        
         print(q)
-        
+        print('-'*50)
         res = query(q) # limit your results in templates.py before testing!
         for entry in res["results"]["bindings"]:
-            for key in entry:
-                print(key, entry[key]["value"])
+            concept = entry['concept']['value']
+            concept_label = entry['conceptLabel']['value']
+            peripheral_concept = entry['peripheralConcept']['value']
+            peripheral_concept_label = entry['peripheralConceptLabel']['value']
+            print(concept, concept_label, peripheral_concept, peripheral_concept_label)
 
 if __name__ == '__main__':
     download_all_data()
