fba7e9b51dc62d612ca35251ce078f8247d84ee1
Mars <marshuang80@gmail.com>
2020-05-28 22:33:22 -0700

    [EDIT] final edits for nearest neighbors: simplified code, fixed aggregated output format, pca zero-center, distance metrics

--- module/get_k_neighbors.sh
+++ module/get_k_neighbors.sh
@@ -1,6 +1,6 @@
-# test on ADHD medications
-python3 ./queries/nearest_neighbors.py --targets "['Q191924','Q1706418','Q179452','Q1706418','Q2506823']" \
-                                     --emb_file-path ./embeddings.emb \
+python3 ./queries/nearest_neighbors.py --targets "['Q191924', 'Q179452']" \
+                                     --emb_file-path ./ent_emb.json \
                                      --n_neighbors 5 \
-                                     --algorithm cosine_similarity \
-                                     save_neighbors
+                                     --distance_metric cos\
+                                     --aggregate_method nearest\
+                                     get_neighbors
