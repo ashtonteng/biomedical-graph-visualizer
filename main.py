7914eaa3825a88d800a81d76d93aa108eec16572
Derek <derek.jow@gmail.com>
2020-06-02 20:05:48 -0700

    Security and favicon
    Added some try except statements in the backend to prevent crashing; added a favicon!

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, Response, jsonify
+from flask import Flask, render_template, request, Response, jsonify, send_from_directory
 from module.subgraph_tool.subgraph import subgraph_tool
 from module.similarity_tool.nearest_neighbors import NearestNeighbors
 from module.graph_lib.constants import *
@@ -11,6 +11,10 @@ app = Flask(__name__, static_folder='static')
 def main():
     return render_template("home.html")
 
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
 @app.route('/tool1')
 def tool1():
@@ -29,7 +33,10 @@ def graph_search():
     hops = request.form.get('hops', default = 3, type = int)
     mode = request.form.get('mode', default = 0, type = int)
 
-    results = subgraph_tool(starting_node, ending_node, hops, max_results=500, mode=mode)
+    try:
+        results = subgraph_tool(starting_node, ending_node, hops, max_results=500, mode=mode)
+    except:
+        return Response("{'message': 'the data was malformed in some way'}", status=400, mimetype='application/json')
 
     return jsonify(results)
 
@@ -38,17 +45,30 @@ def graph_search():
 def similarity_search():
     data = request.get_json()
     if "targets" not in data:
-        return Response("{'message': 'No targets found'}", status=403, mimetype='application/json')
+        return Response("{'message': 'No targets found'}", status=400, mimetype='application/json')
+    if "mode" not in data:
+        return Response("{'message': 'No mode found'}", status=400, mimetype='application/json')
+    if "k" not in data:
+        return Response("{'message': 'No k found'}", status=400, mimetype='application/json')
+    if "concept" not in data:
+        return Response("{'message': 'No concept found'}", status=400, mimetype='application/json')
 
     targets = data["targets"]
-    mode = int(data["mode"])
-    k = int(data["k"])
     concept = data["concept"]
 
-    if mode == 0:
-        knn = NearestNeighbors(targets, concept=concept, n_neighbors=k)
-    else:
-        knn = NearestNeighbors(targets, concept=concept, n_neighbors=k, aggregate_method="nearest")
+    try:
+        mode = int(data["mode"])
+        k = int(data["k"])
+    except:
+        return Response("{'message': 'k or mode is not an integer'}", status=400, mimetype='application/json')
+
+    try:
+        if mode == 0:
+            knn = NearestNeighbors(targets, concept=concept, n_neighbors=k)
+        else:
+            knn = NearestNeighbors(targets, concept=concept, n_neighbors=k, aggregate_method="nearest")
+    except:
+        return Response("{'message': 'knn failed due to unknown values'}", status=400, mimetype='application/json')
 
     results = knn.get_neighbors()
 
