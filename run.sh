fa4550edc76a5b623d017c2727c54c6935a4c69b
blanca <blanca@cs.stanford.edu>
2020-08-21 14:38:12 -0700

    Add relationPrediction to run script

--- run.sh
+++ run.sh
@@ -2,6 +2,9 @@
 DATA_DIR="/data4/blanca/data/wikidata-bmi/"
 # Location of the STransE executable relative to the runpath of this script
 STRANSE="../STransE/STransE"
+REL_DATA_DIR="/data4/blanca/data/wikidata-bmi/relation/"
+OUT_DIR="/data4/blanca/checkpoints/wikidata-bmi/out/"
+RELATION_PREDICTION="../relationPrediction/main.py"
 
 # First, remove stale files
 rm ./module/graph_lib/download/*.tsv
@@ -16,12 +19,7 @@ python3 ./module/similarity_tool/preprocessing.py \
 
 # Go to STransE directory (sibling of this repo) and run
 # STransE must already be built from src in order for this to run properly
-# TODO: Add STransE 
- $STRANSE -model 1 -data $DATA_DIR -size 50 -margin 5 -l1 1 -lrate 0.0005 -init 0
+$STRANSE -model 1 -data $DATA_DIR -size 50 -margin 5 -l1 1 -lrate 0.0005 -init 0
 
-# python3 ./queries/nearest_neighbors.py --targets "['Q191924', 'Q179452']" \
-#                                      --emb_file-path ./ent_emb.json \
-#                                      --n_neighbors 5 \
-#                                      --distance_metric cos\
-#                                      --aggregate_method nearest\
-#                                      get_neighbors
+# Run relation embedding train script
+python $RELATION_PREDICTION --data $REL_DATA_DIR --get_2hop True --partial_2hop True --margin 1 --epochs_gat 1000 --epochs_conv 100 --weight_decay_gat 0.00001 --out_channels 50 --drop_conv 0.3 --output_folder $OUT_DIR --entity_out_dim 25 50
