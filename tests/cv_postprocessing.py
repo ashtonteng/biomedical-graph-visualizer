ae7cc7c68128d0da28d7632ddee2e465cb88c03b
blanca <blanca@cs.stanford.edu>
2021-01-09 18:01:52 -0800

    Fix linter issues

--- tests/cv_postprocessing.py
+++ tests/cv_postprocessing.py
@@ -8,18 +8,12 @@
 6. Create embeddings for each CV graph
 7. Evaluate NN results
 """
-import copy
 import os
-import re
 import pandas as pd
 import numpy as np
 
-from sklearn import model_selection
-from test_constants import TEST_CONDITIONS, TREATS
-from module.graph_lib import graph_utils
+from test_constants import TEST_CONDITIONS
 from module.graph_lib.constants import DRUG_ID
-from module.graph_lib.graph import Graph
-from module.similarity_tool.preprocessing import preprocess_graph_for_relation_prediction
 from module.subgraph_tool.subgraph import get_graph
 
 N_FOLDS = 5
@@ -32,20 +26,20 @@ def load_cv_embeddings(cond, fold):
     # End debugging params
     eemb = np.loadtxt(os.path.join(fold_data_path, 'entity2vec.txt'))
     eids = pd.read_csv(os.path.join(fold_data_path, 'entity2id.txt'), header=None, delimiter='\t')
-    labels = pd.read_csv(fold_data_path + '_test_instance_ids.txt', header=None)    
+    labels = pd.read_csv(fold_data_path + '_test_instance_ids.txt', header=None)
     labels = labels.values.flatten()
-    n_test = len(labels) 
+    n_test = len(labels)
     rids = pd.read_csv(os.path.join(fold_data_path, 'relation2id.txt'), header=None, delimiter='\t')
     remb = pd.read_csv(os.path.join(fold_data_path, 'relation2vec.txt'), header=None, delimiter='\t')
     remb = remb.values[:, :-1]
     treated_by_id = rids[rids[0] == 'P2176'][1]  # Get the relation vec ID
     treated_by_emb = remb[treated_by_id]
     cond_vec = eemb[np.where(eids[0].values == cond['ID'])]
-    cond_treated_by_vec = cond_vec + treated_by_emb 
+    cond_treated_by_vec = cond_vec + treated_by_emb
     # Get pairwise distances between the cond_treated_by_vec and all entity embeddings in the graph
     distances = np.linalg.norm(eemb - cond_treated_by_vec, axis=0)
     # Option 1: Use a distance threshold
-    indices = np.argwhere(distances < 50).flatten()
+    # indices = np.argwhere(distances < 50).flatten()
     # Option 2: Get the indices of the k smallest distances
     # Set k to be the number of test examples for the cross-validation
     k = n_test
@@ -68,19 +62,19 @@ def load_cv_embeddings(cond, fold):
         k += 1
     # Now compute performance metrics
     all_drugs_used_for_treatment = cond['DrugIDs']
-    train_drugs = set(all_drugs_used_for_treatment).difference(set(labels))
     print(drugs_found)
     acc = len(set(drugs_found).intersection(all_drugs_used_for_treatment)) / float(len(labels))
     print("Accuracy for {} fold {}: {}".format(cond['Name'], fold, acc))
+
 
 if __name__ == "__main__":
     # Load all of the embeddings related to conditions to test
     for cond in TEST_CONDITIONS:
         for fold in range(N_FOLDS):
-            load_cv_embeddings(cond, fold) 
+            load_cv_embeddings(cond, fold)
         break  # TODO: Remove this break if we want to test more conditions
     # Load embeddings
     # Load train indices/node instance IDs
     # Load test indices/node instance IDs
     # Compute KNN
-    # Compute sensitivity  
+    # Compute sensitivity
